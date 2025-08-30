273f0290df5c9b0b800297dbdb69870ff288ea71
viscatommy <tommy21heel@gmail.com>
2025-08-31 00:35:40 +0900

    added context help by JavaScript

--- app.js
+++ app.js
@@ -9,3 +9,19 @@ emailInput.placeholder = "name@company.com"
 
 const passwordInput = document.getElementById("password");
 passwordInput.placeholder = "••••••••";
+
+const contextHelp = document.getElementById("contexthelp");
+const contextHelp1 = document.getElementById("contexthelp1");
+
+emailInput.addEventListener("focus", () => {
+    contextHelp.style.display = "block";
+});
+emailInput.addEventListener("blur", () => {
+    contextHelp.style.display = "none";
+});
+passwordInput.addEventListener("focus", () => {
+    contextHelp1.style.display = "block";
+});
+passwordInput.addEventListener("blur", () => {
+    contextHelp1.style.display = "none";
+});
