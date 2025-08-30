3eec53275d5175b7e9bdcd06f60f48d0f66eda07
viscatommy <tommy21heel@gmail.com>
2025-08-30 22:42:24 +0900

    JavaScript placeholder was added

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 window.addEventListener("load", () => {
     setTimeout(() => {
-        alert("ボタンのどれかをクリックするとイベントが発生します");
+        alert("ボタンのどれかをクリックするとイベントが発生するかも");
     }, 1000);
 });
+
+const emailInput = document.getElementById("youremail");
+emailInput.placeholder = "name@company.com"
+
+const passwordInput = document.getElementById("password");
+passwordInput.placeholder = "••••••••";
